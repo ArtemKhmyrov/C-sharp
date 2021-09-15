8ada28aef7d050e25dddca4d7105a24cce47f78c
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-09-15 19:59:55 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 
+            // int a = 23, b = 43; 
+            // Console.WriteLine(a + b);
+
+
+            // 2
+            // int a = 12, b = 34;
+            // Console.WriteLine(a + "+" + b + "=" + b + "+" + a);
+
+
+            // 3
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // int b = Convert.ToInt32(Console.ReadLine());
+            // int c = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(a + b + c);
+
+            //4
+            // double a = double.Parse(Console.ReadLine());
+            // double b = double.Parse(Console.ReadLine());
+            // Console.WriteLine("{0}*{1}={2:f1}", a, b, a * b);
+
+            //5
+            // float a = float.Parse(Console.ReadLine());
+            // float b = float.Parse(Console.ReadLine());
+            // Console.WriteLine("{0}/{1}={2:f3}", a, b, a / b);
+
+            //6
+            // float a = float.Parse(Console.ReadLine());
+            // float b = float.Parse(Console.ReadLine());
+            // float c = float.Parse(Console.ReadLine());
+            // Console.WriteLine("<{0:.##}+{1:.##}>+{2:.##} = {0:.##}+<{1:.##}+{2:.##}>", a, b, c);
+         
+        }
+    }
+}
