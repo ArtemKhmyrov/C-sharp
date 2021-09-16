310f4f5d0399f3a52c78c594e09188c1ce68abaa
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-09-17 01:50:57 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// 1 
+            // int a = 23, b = 43; 
+            // Console.WriteLine(a + b);
+
+            //// 2
+            // int a = 12, b = 34;
+            // Console.WriteLine(a + "+" + b + "=" + b + "+" + a);
+
+            //// 3
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // int b = Convert.ToInt32(Console.ReadLine());
+            // int c = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(a + b + c);
+
+            ////4
+            // double a = double.Parse(Console.ReadLine());
+            // double b = double.Parse(Console.ReadLine());
+            // Console.WriteLine("{0}*{1}={2:f1}", a, b, a * b);
+
+            ////5
+            // float a = float.Parse(Console.ReadLine());
+            // float b = float.Parse(Console.ReadLine());
+            // Console.WriteLine("{0}/{1}={2:f3}", a, b, a / b);
+
+            ////6
+            // float a = float.Parse(Console.ReadLine());
+            // float b = float.Parse(Console.ReadLine());
+            // float c = float.Parse(Console.ReadLine());
+            // Console.WriteLine("<{0:.##}+{1:.##}>+{2:.##} = {0:.##}+<{1:.##}+{2:.##}>", a, b, c);
+
+            ////7
+            // String nomcup = "Номинал купюры = ";
+            // int a = int.Parse(Console.ReadLine());
+            // String kolcup = "Количество купюр = ";
+            // int b = int.Parse(Console.ReadLine());
+            // Console.WriteLine(nomcup + a + "\n" + kolcup + b + "\n" + "Сумма денег = " + (a * b));
+
+            ////8
+            // Console.WriteLine("Введите сумму вклада: ");
+            // double a = double.Parse(Console.ReadLine());
+            // Console.WriteLine("Введите процент по вкладу: ");
+            // double b = double.Parse(Console.ReadLine());
+            // double result = ((a / 100) * b);
+            // Console.WriteLine("Сумма вклада = " + a + "\n" + "Процент по вкладу = " + b + "\n" + "Через год начислят: " +  Math.Round(result, 2) + "р.");
+
+            ////9
+            // Console.WriteLine("Введите сумму вклада: ");
+            // double a = double.Parse(Console.ReadLine());
+            // Console.WriteLine("Введите процент по вкладу: ");
+            // double b = double.Parse(Console.ReadLine());
+            // double result = ((a / 100) * b + a);
+            // Console.WriteLine("Сумма вклада = " + a + "\n" + "Процент по вкладу = " + b + "\n" + "Через год сумма на вкладе: " +  Math.Round(result, 2) + "р.");
+
+            ////10
+            //Console.WriteLine("Как тебя зовут? ");
+            //String name = Console.ReadLine();
+            //Console.WriteLine("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //DateTime thisYear = DateTime.Today;
+            //Console.WriteLine(name + ", ты родился в " + (thisYear.Year - age) + " году");
+            
+
+
+        }
+    }
+}
