525931c0c1df6fa3f96628bc44b52ea4ec6dbabe
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-09-29 13:01:44 +0400

    Add files via upload

---
+++ Eighth_modul(3_20).cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            String s = Console.ReadLine();
+            massage(s);
+        }
+        static char[] splitter = { ' ', '[', ']', '(', ')', '{', '}', '*', ',', '.', ';', ':', '\u2026', '#', '=' }; // это для разбиения на классы и на оссмысленные слова
+        static void massage (String s)
+        {
+            Console.WriteLine(s);
+            String[] words = s.Split(splitter);
+            Object[,] array = new Object[2, words.Length];
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                array[0, i] = words[i].Length; 
+                array[1, i] = words[i];     
+                if (words[i].Length < min)
+                {
+                    min = words[i].Length; 
+                }
+                if (words[i].Length > max)
+                {
+                    max = words[i].Length; 
+                }
+            }
+
+            for (int j = min; j <= max; j++)
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if ((int)array[0, i] == j) 
+                    {
+                        Console.WriteLine("({0}) {1}", array[0, i], array[1, i]); 
+                    }
+                }
+            }
+        }
+    }
+}
