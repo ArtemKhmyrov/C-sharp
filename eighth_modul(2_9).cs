073e82f23706d95031e6e90a99e01e449f6e0e67
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-09-29 12:37:44 +0400

    Add files via upload

---
+++ eighth_modul(2_9).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Eighth_modul
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите s: ");
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Введите str1: ");
+            StringBuilder str1 = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Введите str2: ");
+            StringBuilder str2 = new StringBuilder(Console.ReadLine());
+            while (s.ToString().IndexOf(str1.ToString()) != -1)
+            {
+                s.Replace(str1.ToString(), str2.ToString());
+                Console.WriteLine("Измененная строка s: {0}", s);
+            }   
+        }
+    }
+}
