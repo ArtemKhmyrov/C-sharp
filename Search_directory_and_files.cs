772e14d32f7c0445b494bb2dbcb2744c323ae2b7
Artem <50808797+ArtemKhmyrov@users.noreply.github.com>
2021-10-13 17:56:09 +0400

    Add files via upload

---
+++ Search_directory_and_files.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // для быстрого примера "C:\Users\ahmyr\OneDrive\Документы\Учеба\Графика С++" и "*docx"
+            string directory = Console.ReadLine();
+            string fileName = Console.ReadLine();
+
+            foreach (string findedFile in Directory.EnumerateFiles(directory, fileName,
+                SearchOption.AllDirectories))
+            {
+                FileInfo FI;
+                try
+                {
+                    FI = new FileInfo(findedFile);
+                    Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + "_байт" +
+                        " Создан: " + FI.CreationTime);
+                }
+                catch 
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
